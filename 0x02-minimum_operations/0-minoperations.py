d23d3cb6e9c85e67e96ab10dbca70cfa3f54e18e
opegol <139570017+opegol@users.noreply.github.com>
2024-07-11 22:32:48 +0100

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,27 +1,24 @@
 #!/usr/bin/python3
 
-""Method to devise a solution that efficiently calculates
-the minimum number of operations to achieve a given number
-of characters using only “Copy All” and “Paste” operations. 
+"""Method to devise a solution that efficiently calculates
+   the minimum number of operations to achieve a given number
+   of characters using only “Copy All” and “Paste” operations. 
 """
 
 
 def minOperations(n):
-"""calculates the fewest number of operations needed to result in exactly n H characters"""
-    if n <= 1:
-        return 0
-    
+"""calculates the fewest number of operations"""
     ops = 0
-    current = 1  # current number of Hs in the file
+    current = 1 # current number of Hs in the file
+    counter = 0
     
-    for d in range(2, n + 1):
-        while n % d == 0:
-            ops += d
-            n //= d
-            current *= d
-    
-    if n > 1:
-        return 0  # If we have remainder left, n wasn't completely factorized to 1
-    
-    return ops
-  
+    while now < n:
+        rem = n - current 
+        if (rem % current == 0):
+            ops = current
+            current += ops
+            counter += 2
+        else:
+            current += ops
+            counter += 1
+    return counter
