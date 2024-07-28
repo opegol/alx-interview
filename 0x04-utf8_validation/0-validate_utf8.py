5e88f8f24e0ccd6cb139064d90b1723ce28b4555
opegol <opgbenga@gmail.com>
2024-07-28 22:28:08 +0000

    first commit for 0-validate_utf8 project

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Defines a UTF-8 Validation method"""
+
+
+def validUTF8(data):
+    """Method determines if a given data set
+    represents a valid UTF-8 encoding
+    """
+    num_bytes = 0
+
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
+
+    for i in data:
+
+        mask_byte = 1 << 7
+
+        if num_bytes == 0:
+
+            while mask_byte & i:
+                num_bytes += 1
+                mask_byte = mask_byte >> 1
+
+            if num_bytes == 0:
+                continue
+
+            if num_bytes == 1 or num_bytes > 4:
+                return False
+
+        else:
+            if not (i & mask_1 and not (i & mask_2)):
+                return False
+
+        num_bytes -= 1
+
+    if num_bytes == 0:
+        return True
+
+    return False
---
+++ 0x04-utf8_validation/README.md
@@ -0,0 +1,1 @@
+Readme file for 0x04-utf8_validation project
