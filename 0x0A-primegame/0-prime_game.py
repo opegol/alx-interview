8a113fbec1c8139a23134b90b6bfa5c4ee77bd95
opegol <opgbenga@gmail.com>
2024-09-04 19:47:36 +0000

    First commit for 0x0A-primegame project

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Playing a Prime Game.
+"""
+
+
+def soePrimes(n):
+    """Return list of prime numbers between 1 and n inclusive
+        using Sieve of Eratosthenes.
+    """
+    primeArr = [True] * (n + 1)
+    primeArr[0] = primeArr[1] = False
+    p = 2
+    while (p * p <= n):
+        if (primeArr[p]):
+            for i in range(p * p, n + 1, p):
+                primeArr[i] = False
+        p += 1
+    primes = [i for i in range(2, n + 1) if primeArr[i]]
+    return primes
+
+
+def isWinner(x, nums):
+    """
+    Returns the winner of a Prime Game
+    Args:
+        x (int): no. of rounds of prime game
+        nums (int): array of upper bound n of each round where n is a set of
+                consecutive integers starting from 1 up to and including n
+    Return:
+        Name of player (Maria or Ben) that wins the most rounds or
+        None if winner cannot be determined.
+    """
+    if x is None or nums is None or x == 0 or nums == []:
+        return None
+    Maria = 0
+    Ben = 0
+    for i in range(x):
+        primes = soePrimes(nums[i])
+        # print(primes)
+        if len(primes) % 2 != 0:
+            Maria += 1
+        else:
+            Ben += 1
+    if Maria == Ben:
+        return None
+    else:
+        return "Ben" if Ben > Maria else "Maria"
---
+++ 0x0A-primegame/main_0.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+isWinner = __import__('0-prime_game').isWinner
+
+
+# print("Winner: {}".format(isWinner(4, [13, 17, 14, 6])))
+print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
