484c198a8d74a416d1e4c9726c934624b96e7869
opegol <opgbenga@gmail.com>
2024-09-04 21:12:27 +0000

    Error correction commit for 0x0A-primegame project

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -8,7 +8,6 @@ def soePrimes(n):
         using Sieve of Eratosthenes.
     """
     primeArr = [True] * (n + 1)
-    primeArr[0] = primeArr[1] = False
     p = 2
     while (p * p <= n):
         if (primeArr[p]):
@@ -17,17 +16,7 @@ def soePrimes(n):
         p += 1
     primes = [i for i in range(2, n + 1) if primeArr[i]]
     return primes
-    
-    """
-    primeNos = []
-    filtered = [True] * (n + 1)
-    for prime in range(2, n + 1):
-        if (filtered[prime]):
-            primeNos.append(prime)
-            for i in range(prime, n + 1, prime):
-                filtered[i] = False
-    return primeNos
-    """
+
 
 def isWinner(x, nums):
     """
@@ -39,14 +28,11 @@ def isWinner(x, nums):
     Return:
         Name of player (Maria or Ben) that wins the most rounds or
         None if winner cannot be determined.
-    
-    if x is None or nums is None or x == 0 or nums == []:
-        return None
+    """
     Maria = 0
     Ben = 0
     for i in range(x):
         primes = soePrimes(nums[i])
-        # print(primes)
         if len(primes) % 2 != 0:
             Maria += 1
         else:
@@ -55,18 +41,3 @@ def isWinner(x, nums):
         return None
     else:
         return "Ben" if Ben > Maria else "Maria"
-    """
-    if x is None or nums is None or x == 0 or nums == []:
-        return None
-    Maria = Ben = 0
-    for i in range(x):
-        primeNos = soePrimes(nums[i])
-        if len(primeNos) % 2 == 0:
-            Ben += 1
-        else:
-            Maria += 1
-    if Maria > Ben:
-        return 'Maria'
-    elif Ben > Maria:
-        return 'Ben'
-    return None
--- 0x0A-primegame/main_0.py
+++ 0x0A-primegame/main_0.py
@@ -3,5 +3,5 @@
 isWinner = __import__('0-prime_game').isWinner
 
 
-# print("Winner: {}".format(isWinner(4, [13, 17, 14, 6])))
+# print("Winner: {}".format(isWinner(2, [0, 1])))
 print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
